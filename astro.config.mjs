5534658b8a6b050614d2a239b7064f2e8ab4063c
Kaeldehta <flo@visiolab.io>
2026-03-26 15:33:21 +0100

    go back to static rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-
   adapter: cloudflare({
-    imageService: "cloudflare-binding",
+    imageService: "compile",
   }),
 
   vite: {
